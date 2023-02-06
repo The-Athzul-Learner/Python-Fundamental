f5dbaca84b201e4e704dfb11d48ee5ad31b31954
Athallah <athallah.hartamawangsyah@gmail.com>
2023-02-07 06:31:44 +0700

    Starter

---
+++ Python-Comments-&-Variable.py
@@ -0,0 +1,53 @@
+#COMMENT
+#This is one line comment and below is multiline comment!
+"""Comment can be used to:
+   1. Explain python Code
+   2. Make the code more readable
+   3. Prevent code form execution"""
+
+#Variable
+#Variable are containers for storing data values
+
+#Assign Value
+x="ATHA"
+print(x)
+
+#Get the Type
+x=5
+y=0.4
+z="Atha"
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Variable name
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#Multi Word Variable name
+myVariableName = "Atha" #Camel Case
+MyVariableName = "Atha" #Pascal Case
+my_variable_name = "Atha" #Snake Case
+
+#Assign Multiple Values
+x, y, z = "Orange", "Juice", "Banana"
+print(x)
+print(y)
+print(z)
+#One Value to multiple variable
+x=y=z= "Orange"
+print(x)
+print(y)
+print(z)
+#Output Variable
+x="python"
+y="is"
+z="awesome"
+a=4
+b=7
+print(x, y, z)
+print(a+b)
