542ced5544e7674f1545a43f36c9c8ef52f44ac5
Athallah <athallah.hartamawangsyah@gmail.com>
2023-02-08 06:45:11 +0700

    Starter

--- Data-Types-&-Casting.py
+++ Data-Types-&-Casting.py
@@ -1,3 +1,19 @@
 ##Data Types
 """In programming, data type is an important concept.
 Variables can store data of different types, and different types can do different things."""
+
+#Tipe data = angka (Integer)
+data_integer = 11
+print("Data integer = ",data_integer)
+print("Tipe data :", type(data_integer))
+
+#Tipe data = angka dengan koma (Float)
+data_float = 0.5
+print("Data Float = ", data_float)
+print("Tipe data :", type(data_float))
+
+#Tipe data = complex number (Complex)
+data_complex = 7+8j
+print("Data complex = ",data_complex)
+print("Tipe data :", type(data_complex))
+#
