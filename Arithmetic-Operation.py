544c5aafecee753813c9cfd388913b6e4f9bd71d
Athallah <athallah.hartamawangsyah@gmail.com>
2023-02-08 08:03:10 +0700

    Starter

--- Arithmetic-Operation.py
+++ Arithmetic-Operation.py
@@ -1,1 +1,31 @@
 ##Operasi Aritmatika Standar
+x = 5
+y = 6
+#Operasi Penjumlahan (+)
+hasil =x+y
+print(x, "+", y, "=", hasil )
+#Operasi Pengurangan (-)
+hasil =x-y
+print(x, "-", y, "=", hasil )
+#Operasi Perkalian (*)
+hasil =x*y
+print(x, "x", y, "=", hasil )
+#Operasi Pembagian (/)
+hasil =x/y
+print(x, ":", y, "=", hasil )
+#Operasi Eksponen (**)
+# hasil =x**y
+# print(x, "-", y, "=", hasil ))
+#Operasi Modulus (%)
+hasil =x%y
+print(x, "%", y, "=", hasil )
+#Operasi floor division (//)
+hasil =x//y
+print(x, "//", y, "=", hasil )
+
+#Operational Precedence
+"""1.()
+2.Eksponen
+3.Perkalian, Pembagian, Modulus, Floor division
+4.Penjumblahan dan pengurangan """
+
