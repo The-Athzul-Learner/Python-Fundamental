42aa5ecab8c8eda80e61c0fc8fb9820cf0eeec08
Athallah <athallah.hartamawangsyah@gmail.com>
2023-02-08 08:19:25 +0700

    Starter

---
+++ Temperature-Conversion-Program.py
@@ -0,0 +1,16 @@
+print("TEMPERATURE CONVERSION PPROGRAM")
+
+celcius = float(input("Masukan suhu dalam celcius: "))
+print("Suhu dalam celcius adalah", celcius)
+#Celcius to Reamur
+reamur = (4/5)*celcius
+print("Suhu dalam reamur adalah ", reamur)
+
+#Celcius to Fahrenheit
+fahrenheit = (9/5)*celcius+32
+print("Suhu dalam reamur adalah ", fahrenheit)
+
+#Celcius to Reamur
+kelvin= celcius+273
+print("Suhu dalam reamur adalah ", kelvin)
+
