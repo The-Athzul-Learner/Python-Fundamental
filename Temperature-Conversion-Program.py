e588a99f3822301eaa67f353ead2c2e6cc4b5d32
Athallah <athallah.hartamawangsyah@gmail.com>
2023-02-09 07:51:35 +0700

    Starter

--- Temperature-Conversion-Program.py
+++ Temperature-Conversion-Program.py
@@ -8,9 +8,20 @@ print("Suhu dalam reamur adalah ", reamur)
 
 #Celcius to Fahrenheit
 fahrenheit = (9/5)*celcius+32
-print("Suhu dalam reamur adalah ", fahrenheit)
+print("Suhu dalam fahrenheit adalah ", fahrenheit)
 
-#Celcius to Reamur
+#Celcius to kelvin
 kelvin= celcius+273
-print("Suhu dalam reamur adalah ", kelvin)
+print("Suhu dalam kelvin adalah ", kelvin)
+
+##Homework
+#Converse Fahrenheit to kelvin amd otherwise
+fahrenheit = float(input("Masukan suhu dalam fahrenheit: "))
+fahrenheit_kelvin = 5/9*(fahrenheit-32)+273
+print("Suhu dalam kelvin adalah ", fahrenheit_kelvin)
+
+kelvin = float(input("Masukan suhu dalam kelvin: "))
+kelvin_fahrenheit = 9/5*(kelvin-273)+32
+print("Suhu dalam fahrenheit adalah ", kelvin_fahrenheit)
+
 
