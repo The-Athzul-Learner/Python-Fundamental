b172be5afb9efeb9d67503460c8c80999c0d6339
Athallah <athallah.hartamawangsyah@gmail.com>
2023-02-10 22:53:12 +0700

    Starter

---
+++ 12-Operation-&-Manipulation-of-String.py
@@ -0,0 +1,14 @@
+##Operasi dan manipulasi string
+
+# 1. mengambung string (concatenate)
+nama_pertama = "ucup"
+nama_tengah = "D"
+nama_akhir = "Fame"
+
+nama_lengkap = nama_pertama+" "+nama_tengah+" "+nama_akhir
+print(nama_lengkap)
+
+# 2. Menghitung panjang string
+
+# 3. Operator untuk string
+# Mengecek apakah ada komponen char atau string di string
