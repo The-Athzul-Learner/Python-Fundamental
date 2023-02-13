ac890af91db5cb6782f37ced1a5f45e098bc7a9a
Athallah <athallah.hartamawangsyah@gmail.com>
2023-02-13 08:21:51 +0700

    Starter

--- 16-IF-ELIF-&-ELSE-Statement.py
+++ 16-IF-ELIF-&-ELSE-Statement.py
@@ -29,3 +29,19 @@ else:
 	print("Ah kamu bukan otong, kamu gak keren! :(")
 
 print("akhir dari program")
+
+
+# ELIF = else if statement
+
+nama = input("Nama kamyu siapa? ")
+
+if nama == "ucup": # kondisi 1
+	print("Hai ganteeeeng beuds!") # aksi true 1
+elif nama == "otong": # kondisi 2
+	print("Hai si kece bangeeeets!!") # aksi true 2
+elif nama == "mario": # kondisi 3
+	print("Hai humooooreeeesh!") # aksi true 3
+else:
+	print("au ah gak kenal!!!") # aksi false
+
+print("ini adalah akhir dari program")
